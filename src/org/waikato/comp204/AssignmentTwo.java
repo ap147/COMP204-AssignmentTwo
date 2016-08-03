86e0a15c4b03443dbbbb94e38d8d41c4ffe6b5e0
Parmar, Amarjot <ap147@students.waiakto.ac.nz>
2016-08-03 13:16:21 +1200

    Added Constuctor/ getCapacity() for Scrollback.java

--- src/org/waikato/comp204/AssignmentTwo.java
+++ src/org/waikato/comp204/AssignmentTwo.java
@@ -1,7 +1,9 @@
 package org.waikato.comp204;
 
-public class AssignmentTwo {
-    public static void main(String[] args) {
+public class AssignmentTwo
+{
+    public static void main(String[] args)
+    {
         System.out.println("Code your user interface class here");
     }
 }
--- src/org/waikato/comp204/scrollback/Scrollback.java
+++ src/org/waikato/comp204/scrollback/Scrollback.java
@@ -3,29 +3,44 @@ package org.waikato.comp204.scrollback;
 /**
  * A String Scrollback implementation
  */
-public class Scrollback implements ScrollbackInterface {
+public class Scrollback implements ScrollbackInterface
+{
+    private int elementsCount;
+    public  Scrollback()
+    {
+        elementsCount = 10;
+    }
+    public  Scrollback(int amount)
+    {
+        elementsCount = amount;
+    }
     @Override
-    public void add(String item) {
+    public void add(String item)
+    {
 
     }
 
     @Override
-    public String getLast() {
+    public String getLast()
+    {
         return null;
     }
 
     @Override
-    public void clear() {
+    public void clear()
+    {
 
     }
 
     @Override
-    public int getCapacity() {
-        return 0;
+    public int getCapacity()
+    {
+        return elementsCount;
     }
 
     @Override
-    public int getCount() {
+    public int getCount()
+    {
         return 0;
     }
 }
